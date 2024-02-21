c3cec355acf9803df8c6133ecf3a9a668ecc49ec
안철민 <81482706+cjfals9626@users.noreply.github.com>
2024-02-21 15:50:54 +0900

    환경세팅 완료

---
+++ src/main/java/org/spring/pizzarazzi/PizzarazziApplication.java
@@ -0,0 +1,14 @@
+package org.spring.pizzarazzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzarazziApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzarazziApplication.class, args);
+        System.out.println();
+    }
+
+}
